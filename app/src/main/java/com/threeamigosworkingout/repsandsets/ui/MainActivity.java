bcf660dc2e929a15dea542da2a79ee2acc348825
chan-kelv <chan.kelv@gmail.com>
2021-11-09 00:50:55 -0800

    added icons

--- app/src/main/java/com/threeamigosworkingout/repsandsets/ui/MainActivity.java
+++ app/src/main/java/com/threeamigosworkingout/repsandsets/ui/MainActivity.java
@@ -1,15 +1,16 @@
 package com.threeamigosworkingout.repsandsets.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static com.threeamigosworkingout.repsandsets.R.layout.activity_main;
 
-import android.content.Context;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(com.threeamigosworkingout.repsandsets.R.layout.activity_main);
+        setContentView(activity_main);
     }
 }
